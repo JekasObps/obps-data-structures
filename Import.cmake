249debf2a574d6ea762006fcc3ced6d65c4dde70
JekasObps <jekas.obps@gmail.com>
2021-09-11 19:34:01 +0300

    + load_submodule moved to build_scripts

--- Import.cmake
+++ Import.cmake
@@ -1,9 +1,8 @@
-
-include(FetchContent)
-
-
-# Inports build infrastructure essentials 
+### 
+# Import common scripts for setup build and testing 
+###
 function(IMPORT_BUILD_SCRIPTS)
+    include(FetchContent)
     FetchContent_Declare(
         build_scripts
         GIT_REPOSITORY https://github.com/JekasObps/build-scripts.git
