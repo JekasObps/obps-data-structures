d4ff73ba093e7fd8bfcb514807bf10ce0c6f9dde
JekasObps <jekas.obps@gmail.com>
2021-09-28 16:41:49 +0300

    switching to async

--- ds/thread_pool/src/thread_pool.hpp
+++ ds/thread_pool/src/thread_pool.hpp
@@ -1,19 +1,23 @@
 #pragma once
 
+#include "spinlock.hpp"
+
 #include <forward_list>
 #include <vector>
-#include <thread>
 #include <future>
 #include <functional>
-
 #include <mutex>
 #include <iostream>
-
-#include "spinlock.hpp"
 
 namespace obps
 {
 
+//
+// This class is more of a container for threads than a pool, it does not aggregates queue.
+// What it actually does: it ensures thread-safe task insertion. Runs user provided thread in a task wrapper.
+// Additionally, ShutDown method provided that locks thread pool until all tasks will finish their execution.
+// And there is no way to force task to exit.
+//
 template <
     typename ThreadRet, // user defined status enum that pool will use to indicate a state of thread execution
     ThreadRet running,  // continue looping on user_thread_func
@@ -33,9 +37,7 @@ public:
     }
 
     template <typename ...Args> 
-    void RunTask( std::function<ThreadRet(Args...)> user_thread_func, // user task
-        Args ...args // user arguments to pass into the task
-    ) noexcept;
+    void RunTask(std::function<ThreadRet(Args...)> user_thread_func, Args ...args) noexcept;
 
     // stop all tasks and wait for them
     void ShutDown() noexcept;
@@ -44,9 +46,10 @@ private:
     PoolState m_State;
 
     template <typename ...Args>
-    void PoolThread(std::function<ThreadRet(Args...)>, std::promise<ThreadRet>&, std::future<ThreadRet>&, Args...);
+    void PoolThread(std::function<ThreadRet(Args...)>, Args...);
 
-    std::forward_list<std::tuple<std::promise<ThreadRet>, std::future<ThreadRet>, std::thread>> m_Tasks;
+    std::forward_list<std::future<ThreadRet>> m_Tasks;
+    
     spinlock m_PoolLock;    // prevents race-condition when running tasks and on shutdown
 };
 
@@ -57,40 +60,15 @@ void ThreadPool<ThreadRet, running, finished, aborted>::RunTask(
     std::function<ThreadRet(Args...)> user_thread_func, Args ...args) noexcept 
 {
     std::lock_guard<spinlock> lock(m_PoolLock);
-    
-    std::promise<ThreadRet> promise;
-    auto future = promise.get_future();
 
-    m_Tasks.push_front(std::make_tuple(std::move(promise), std::move(future), std::thread()));
-    auto &[p, f, thread] = m_Tasks.front();
-
-    // starting thread
-    thread = std::thread(
-        &ThreadPool::PoolThread<Args...>, this, 
-        user_thread_func, 
-        std::ref(p), 
-        std::ref(f), 
-        std::forward<Args>(args)...
-    );
+    m_Tasks.push_front(std::async<std::function<ThreadRet(Args...)>, Args...>(std::launch::async, std::move(user_thread_func), std::forward<Args>(args)...));
 }
 
 template <typename ThreadRet, ThreadRet running, ThreadRet finished, ThreadRet aborted>
 template <typename ...Args> 
 void ThreadPool<ThreadRet, running, finished, aborted>::PoolThread( 
-    std::function<ThreadRet(Args...)> user_thread_func,
-    std::promise<ThreadRet>& promise, 
-    std::future<ThreadRet>& future,
-    Args ...args)
+    std::function<ThreadRet(Args...)> user_thread_func, Args ...args)
 {
-    auto return_exception = [&promise](const std::exception& e){
-        try {
-            promise.set_exception(std::make_exception_ptr(e));
-        } 
-        catch(...) {
-            std::cerr << "Exception was thrown on promise::set_exception:\n";
-        }
-    };
-
     for(;;) {
         ThreadRet status;
         do { // wait until thread completes it's work
@@ -99,17 +77,12 @@ void ThreadPool<ThreadRet, running, finished, aborted>::PoolThread(
             }
             catch (const std::exception& e) {
                 std::cerr << "Exception on thread shutdown:\n" << e.what();
-                return_exception(e);
-                return;
+                throw e;
             }
         } while(status == running);
 
-        // return status and exit thread function
-        promise.set_value_at_thread_exit(status);
-        return;
+        return status;
     } // for(;;)
-
-    promise.set_value_at_thread_exit(finished);
 }
 
 
@@ -120,16 +93,12 @@ void ThreadPool<ThreadRet, running, completed, aborted>::ShutDown() noexcept
     // state propagates to all threads
     m_State = PoolState::SHUTDOWN;
     
-    for (auto current = m_Tasks.begin(); current != m_Tasks.end(); )
+    for (auto& current : m_Tasks)
     {
-        auto& [_, future, thread] = *current;
-
-        future.get(); // TODO: process
-        thread.join();
-
-        current = std::next(current);
-        m_Tasks.pop_front();
+        current.get(); // TODO: process
     }
+
+    m_Tasks.clear();
 }
 
 } // namespace obps
